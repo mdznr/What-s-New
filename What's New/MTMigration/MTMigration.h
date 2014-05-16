d8b09e02b6e5e549e55086a9b42b819b184eb3c6
Matt Zanchelli <matt@mdznr.com>
2014-05-16 18:51:58 -0400

    Added in code (@c) to documentation of MTMigration header.

--- What's New/MTMigration/MTMigration.h
+++ What's New/MTMigration/MTMigration.h
@@ -12,16 +12,16 @@ typedef void (^MTExecutionBlock)(void);
 
 @interface MTMigration : NSObject
 
-///	Executes a block of code for a specific version number and remembers this version as the latest migration done by MTMigration.
+///	Executes a block of code for a specific version number and remembers this version as the latest migration done by @c MTMigration.
 /// @param version A string with a specific version number.
-/// @param migrationBlock A block object to be executed when the application version matches the string 'version'. This parameter can't be nil.
+/// @param migrationBlock A block object to be executed when the application version matches the string @c version. This parameter can't be @c nil.
 + (void)migrateToVersion:(NSString *)version block:(MTExecutionBlock)migrationBlock;
 
-///	Executes a block of code for every time the application version changes.
-///	@param updateBlock A block object to be executed when the application version changes. This parameter can't be nil.
+///	Executes a block of code for every time the application version increases.
+///	@param updateBlock A block object to be executed when the application version increases. This parameter can't be @c nil.
 + (void)applicationUpdateBlock:(MTExecutionBlock)updateBlock;
 
-///	Clears the last migration remembered by MTMigration. Causes all migration to run from the beginning.
+///	Clears the last migration remembered by @c MTMigration. Causes all migration to run from the beginning.
 + (void)reset;
 
 @end
