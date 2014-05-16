cdd0f1b96e5542773308f6e8ebaf37dddfe840bd
Matt Zanchelli <matt@mdznr.com>
2014-05-16 18:37:46 -0400

    Cleaning up MTMigration header and implementation.

--- What's New/MTMigration/MTMigration.h
+++ What's New/MTMigration/MTMigration.h
@@ -8,34 +8,20 @@
 
 #import <Foundation/Foundation.h>
 
-
 typedef void (^MTExecutionBlock)(void);
-
 
 @interface MTMigration : NSObject
 
-/** 
- Executes a block of code for a specific version number and remembers this version as the latest migration done by MTMigration.
- 
- @param version A string with a specific version number
- @param migrationBlock A block object to be executed when the application version matches the string 'version'. This parameter can't be nil.
- 
- */
+///	 Executes a block of code for a specific version number and remembers this version as the latest migration done by MTMigration.
+/// @param version A string with a specific version number.
+/// @param migrationBlock A block object to be executed when the application version matches the string 'version'. This parameter can't be nil.
++ (void)migrateToVersion:(NSString *)version block:(MTExecutionBlock)migrationBlock;
 
-+ (void) migrateToVersion:(NSString *)version block:(MTExecutionBlock)migrationBlock;
+///	Executes a block of code for every time the application version changes.
+///	@param updateBlock A block object to be executed when the application version changes. This parameter can't be nil.
++ (void)applicationUpdateBlock:(MTExecutionBlock)updateBlock;
 
-/** 
- 
- Executes a block of code for every time the application version changes.
- 
- @param updateBlock A block object to be executed when the application version changes. This parameter can't be nil.
- 
- */
-
-+ (void) applicationUpdateBlock:(MTExecutionBlock)updateBlock;
-
-/** Clears the last migration remembered by MTMigration. Causes all migration to run from the beginning. */
-
-+ (void) reset;
+///	Clears the last migration remembered by MTMigration. Causes all migration to run from the beginning.
++ (void)reset;
 
 @end
--- What's New/MTMigration/MTMigration.m
+++ What's New/MTMigration/MTMigration.m
@@ -13,71 +13,67 @@ static NSString * const MTMigrationLastAppVersionKey   = @"MTMigration.lastAppVe
 
 @implementation MTMigration
 
-+ (void) migrateToVersion:(NSString *)version block:(MTExecutionBlock)migrationBlock {
+#pragma mark - Public API
+
++ (void)migrateToVersion:(NSString *)version block:(MTExecutionBlock)migrationBlock
+{
 	// version > lastMigrationVersion && version <= appVersion
-    if ([version compare:[self lastMigrationVersion] options:NSNumericSearch] == NSOrderedDescending &&
-        [version compare:[self appVersion] options:NSNumericSearch]           != NSOrderedDescending) {
-		
+    if ( [version compare:[self lastMigrationVersion] options:NSNumericSearch] == NSOrderedDescending &&
+         [version compare:[self appVersion] options:NSNumericSearch]           != NSOrderedDescending ) {
             migrationBlock();
-		
-            #if DEBUG
-                NSLog(@"MTMigration: Running migration for version %@", version);
-            #endif
-		
+			#if DEBUG
+			NSLog(@"MTMigration: Running migration for version %@", version);
+			#endif
             [self setLastMigrationVersion:version];
-        
-        
-        
 	}
 }
 
-
-+ (void) applicationUpdateBlock:(MTExecutionBlock)updateBlock {
-    if (![[self lastAppVersion] isEqualToString:[self appVersion]]) {
-        
++ (void)applicationUpdateBlock:(MTExecutionBlock)updateBlock
+{
+    if ( ![[self lastAppVersion] isEqualToString:[self appVersion]] ) {
         updateBlock();
-        
-        #if DEBUG
-            NSLog(@"MTMigration: Running update Block");
-        #endif
-        
+		#if DEBUG
+		NSLog(@"MTMigration: Running update Block");
+		#endif
         [self setLastAppVersion:[self appVersion]];
     }
 }
 
-
-
-+ (void) reset {
++ (void)reset
+{
     [self setLastMigrationVersion:nil];
     [self setLastAppVersion:nil];
 }
 
 
+#pragma mark - Private API
 
-
-+ (NSString *)appVersion {
++ (NSString *)appVersion
+{
     return [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
 }
 
-+ (void) setLastMigrationVersion:(NSString *)version {
++ (void)setLastMigrationVersion:(NSString *)version
+{
     [[NSUserDefaults standardUserDefaults] setValue:version forKey:MTMigrationLastVersionKey];
     [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
-+ (NSString *) lastMigrationVersion {
++ (NSString *)lastMigrationVersion
+{
     NSString *res = [[NSUserDefaults standardUserDefaults] valueForKey:MTMigrationLastVersionKey];
-
     return (res ? res : @"");
 }
 
-+ (void)setLastAppVersion:(NSString *)version {
++ (void)setLastAppVersion:(NSString *)version
+{
     [[NSUserDefaults standardUserDefaults] setValue:version forKey:MTMigrationLastAppVersionKey];
     [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
-+ (NSString *) lastAppVersion {
++ (NSString *)lastAppVersion
+{
     NSString *res = [[NSUserDefaults standardUserDefaults] valueForKey:MTMigrationLastAppVersionKey];
-    
     return (res ? res : @"");
 }
 
