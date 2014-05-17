fd08c934aa0c94efd200fe16c1dcb41461f09eff
Matt Zanchelli <matt@mdznr.com>
2014-05-17 02:15:05 -0400

    Beginning to set up labels for features.

--- What's New/MTZWhatsNewViewController.m
+++ What's New/MTZWhatsNewViewController.m
@@ -10,6 +10,22 @@
 
 @interface MTZWhatsNewViewController ()
 
+@property (strong, nonatomic) UILabel *title1;
+@property (strong, nonatomic) UILabel *description1;
+@property (strong, nonatomic) UIImageView *icon1;
+
+@property (strong, nonatomic) UILabel *title2;
+@property (strong, nonatomic) UILabel *description2;
+@property (strong, nonatomic) UIImageView *icon2;
+
+@property (strong, nonatomic) UILabel *title3;
+@property (strong, nonatomic) UILabel *description3;
+@property (strong, nonatomic) UIImageView *icon3;
+
+@property (strong, nonatomic) UILabel *title4;
+@property (strong, nonatomic) UILabel *description4;
+@property (strong, nonatomic) UIImageView *icon4;
+
 @end
 
 @implementation MTZWhatsNewViewController
@@ -27,7 +43,7 @@
 	whatsNewLabel.text = NSLocalizedString(@"What's New", nil);
 	whatsNewLabel.textAlignment = NSTextAlignmentCenter;
 	whatsNewLabel.textColor = [UIColor whiteColor];
-	whatsNewLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:30.0f];
+	whatsNewLabel.font = [UIFont fontWithName:@"HelveticaNeue-Thin" size:30.0f];
 	[self.view addSubview:whatsNewLabel];
 	
 	// Get Started.
@@ -43,6 +59,20 @@
 	[button setTitleColor:[[UIColor whiteColor] colorWithAlphaComponent:0.5f] forState:UIControlStateHighlighted];
 	[button addTarget:self action:@selector(didTapContinueButton:) forControlEvents:UIControlEventTouchUpInside];
 	[self.view addSubview:button];
+	
+	// First item.
+	self.title1 = [[UILabel alloc] initWithFrame:CGRectMake(100, 88, 194, 21)];
+	self.title1.numberOfLines = 1;
+	self.title1.textColor = [UIColor whiteColor];
+	self.title1.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:18.0f];
+	[self.view addSubview:self.title1];
+	self.description1 = [[UILabel alloc] initWithFrame:CGRectMake(100, 108, 194, 34)];
+	self.description1.numberOfLines = 0;
+	self.description1.textColor = [UIColor whiteColor];
+	self.description1.font = [UIFont fontWithName:@"HelveticaNeue-Thin" size:14.0f];
+	[self.view addSubview:self.description1];
+	self.icon1 = [[UIImageView alloc] initWithFrame:CGRectMake(26, 82, 60, 60)];
+	[self.view addSubview:self.icon1];
 }
 
 - (IBAction)didTapContinueButton:(id)sender
