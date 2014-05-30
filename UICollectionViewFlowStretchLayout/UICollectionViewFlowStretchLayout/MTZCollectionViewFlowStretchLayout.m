7301f4843d7733e043b3cd6def8747cb479af5bb
Matt Zanchelli <matt@mdznr.com>
2014-05-30 00:11:41 -0400

    Created layout modeled after `UICollectionViewFlowLayout`.

---
+++ UICollectionViewFlowStretchLayout/UICollectionViewFlowStretchLayout/MTZCollectionViewFlowStretchLayout.m
@@ -0,0 +1,13 @@
+//
+//  MTZCollectionViewFlowStretchLayout.m
+//  UICollectionViewFlowStretchLayout
+//
+//  Created by Matt Zanchelli on 5/29/14.
+//  Copyright (c) 2014 Matt Zanchelli. All rights reserved.
+//
+
+#import "MTZCollectionViewFlowStretchLayout.h"
+
+@implementation MTZCollectionViewFlowStretchLayout
+
+@end
