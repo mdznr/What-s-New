4bea197b847a613ae7f1a6ce5a4e9367976250d7
Matt Zanchelli <matt@mdznr.com>
2015-08-15 14:42:01 -0700

    Apostrophes

--- Classes/MTZWhatsNewViewController/MTZCollectionView.h
+++ Classes/MTZWhatsNewViewController/MTZCollectionView.h
@@ -1,6 +1,6 @@
 //
 //  MTZCollectionView.h
-//  What's New
+//  What’s New
 //
 //  Created by Matt Zanchelli on 5/23/14.
 //  Copyright (c) 2014 Matt Zanchelli. All rights reserved.
@@ -9,7 +9,7 @@
 @import UIKit;
 
 /*
- A @c UICollectionView subclass that automatically enables and disables scrolling depending on whether or not the content fits within the scrollview's frame.
+ A @c UICollectionView subclass that automatically enables and disables scrolling depending on whether or not the content fits within the scrollview’s frame.
  */
 @interface MTZCollectionView : UICollectionView
 
