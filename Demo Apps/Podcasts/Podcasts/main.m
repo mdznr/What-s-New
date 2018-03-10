f332dd233c212a931325b7ee90e537540c5934b1
Matt Zanchelli <matt@mdznr.com>
2018-03-10 14:32:59 -0800

    Updated whitespace

--- Demo Apps/Podcasts/Podcasts/main.m
+++ Demo Apps/Podcasts/Podcasts/main.m
@@ -10,8 +10,7 @@
 
 #import "PDCAppDelegate.h"
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char * argv[]) {
 	@autoreleasepool {
 	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([PDCAppDelegate class]));
 	}
